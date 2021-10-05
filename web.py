25cf91a5c4b55b886637574b81f4a879b4119676
Trystan1 <trystan.jones@altran.com>
2021-10-05 17:19:20 +0100

    Next button implemented on Game page (waiting on improved card before displaying it here)

--- web.py
+++ web.py
@@ -25,8 +25,18 @@ def PlayGame():
     player2 = Player2.getAllData()
     print('Player 1')
     print(*player1, sep="\n")
-    print('Player 2')
-    print(*player2, sep="\n")
+    # print('Player 2')
+    # print(*player2, sep="\n")
+    return render_template('game.html')
+
+
+@app.route("/playgame/nextbutton1")
+def NextButton():
+    Player1, Player2, DiscardPile = InitialiseDecks()
+    NextCard(Player1)
+    player1 = Player1.getAllData()
+    print('Player 1')
+    print(*player1, sep="\n")
     return render_template('game.html')
 
 
