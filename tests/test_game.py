a5ed892877403f13a386f06175f10ddf0098ef2b
Trystan1 <trystan.jones@altran.com>
2021-10-06 16:30:14 +0100

    pokemonTypes function completed and test_games unit test script added for it

---
+++ tests/test_game.py
@@ -0,0 +1,37 @@
+from pokemon_types import *
+
+
+class TestTypes:
+
+    @staticmethod
+    def test_fighting_with_rock():
+
+        # Arrange
+        damageModifier = pokemonTypes('fighting', ['rock'])
+
+        # Assert
+        assert damageModifier == 2
+
+    @staticmethod
+    def test_fighting_with_rock_and_grass():
+        # Arrange
+        damageModifier = pokemonTypes('fighting', ['rock', 'grass'])
+
+        # Assert
+        assert damageModifier == 2
+
+    @staticmethod
+    def test_fighting_with_rock_and_fairy():
+        # Arrange
+        damageModifier = pokemonTypes('fighting', ['rock', 'fairy'])
+
+        # Assert
+        assert damageModifier == 1
+
+    @staticmethod
+    def test_fighting_with_rock_and_fairy_and_ghost():
+        # Arrange
+        damageModifier = pokemonTypes('fighting', ['rock', 'fairy', 'ghost'])
+
+        # Assert
+        assert damageModifier == 0
