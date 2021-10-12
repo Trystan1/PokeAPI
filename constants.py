ec086723d31ed150f84db6976f85b8ab9674e532
harrday <harry.day@altran.com>
2021-10-12 12:08:37 +0100

    Game-page set-up as an intermediate, game now allows 0,1 or 2 human players to play and have created a message to show the attacker's move type.

--- constants.py
+++ constants.py
@@ -4,3 +4,10 @@ if os.getcwd().endswith('tests'):
     DATABASE = '../pokemon_library.db'
 else:
     DATABASE = 'pokemon_library.db'
+
+
+PLAYERCHOICES = {
+    0: ['CPU 1', 'CPU 2'],
+    1: ['Player', 'CPU'],
+    2: ['Player 1', 'Player 2']
+}
